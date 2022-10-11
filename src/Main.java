d5bcdf057ecfbd69ce112b107c3767e976ef34db
Edson Junior <edson.junior@hmit.com.br>
2022-10-11 13:13:39 -0300

    Ajuste na formatação

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,44 @@
 public class Main {
-    
-    public static void main(String[] args) {
 
-	/**
-	 * Criação de 3 usuários
-	 * 
-	 */
-	ItemPermissao itemPermissao1 = new Usuario(1l, "Usuário 1");
-	ItemPermissao itemPermissao2 = new Usuario(2l, "Usuário 2");
-	ItemPermissao itemPermissao3 = new Usuario(3l, "Usuário 3");
-	
-	
-	/**
-	 * Criação de 3 grupos. O itemPermissao5 é um grupo 
-	 * formado pelo grupo itemPermissao4 e pelo usuário itemPermissao3, portanto forma uma árvore
-	 * 
-	 */
-	ItemPermissao itemPermissao4 = new Grupo(1l, "Grupo 4", itemPermissao1, itemPermissao2);
-	ItemPermissao itemPermissao5 = new Grupo(1l, "Grupo 5", itemPermissao4, itemPermissao3);
-	ItemPermissao itemPermissao6 = new Grupo(1l, "Grupo 6", itemPermissao3);
-	
-	
-	/**
-	 * Permissão TESTE1 concedido para o usuário itemPermissao1
-	 */
-	itemPermissao1.concederPermissao("TESTE0");
-	
-	/**
-	 * Permissão TESTE1 concedido para o grupo itemPermissao4
-	 */
-	itemPermissao4.concederPermissao("TESTE1");
-	
-	
-	/**
-	 * Permissão TESTE2 concedido para o grupo itemPermissao5, consequentemente
-	 * para todos os usuários da árvore
-	 *
-	 */
-	itemPermissao5.concederPermissao("TESTE2");
-	
-	
-	/**
-	 * Permissão TESTE3 concedido para o grupo itemPermissao6
-	 */
-	itemPermissao6.concederPermissao("TESTE3");
-    }
+	public static void main(String[] args) {
+
+		/**
+		 * Criação de 3 usuários
+		 * 
+		 */
+		ItemPermissao itemPermissao1 = new Usuario(1l, "Usuário 1");
+		ItemPermissao itemPermissao2 = new Usuario(2l, "Usuário 2");
+		ItemPermissao itemPermissao3 = new Usuario(3l, "Usuário 3");
+
+		/**
+		 * Criação de 3 grupos. O itemPermissao5 é um grupo formado pelo grupo
+		 * itemPermissao4 e pelo usuário itemPermissao3, portanto forma uma árvore
+		 * 
+		 */
+		ItemPermissao itemPermissao4 = new Grupo(1l, "Grupo 4", itemPermissao1, itemPermissao2);
+		ItemPermissao itemPermissao5 = new Grupo(1l, "Grupo 5", itemPermissao4, itemPermissao3);
+		ItemPermissao itemPermissao6 = new Grupo(1l, "Grupo 6", itemPermissao3);
+
+		/**
+		 * Permissão TESTE1 concedido para o usuário itemPermissao1
+		 */
+		itemPermissao1.concederPermissao("TESTE0");
+
+		/**
+		 * Permissão TESTE1 concedido para o grupo itemPermissao4
+		 */
+		itemPermissao4.concederPermissao("TESTE1");
+
+		/**
+		 * Permissão TESTE2 concedido para o grupo itemPermissao5, consequentemente para
+		 * todos os usuários da árvore
+		 *
+		 */
+		itemPermissao5.concederPermissao("TESTE2");
+
+		/**
+		 * Permissão TESTE3 concedido para o grupo itemPermissao6
+		 */
+		itemPermissao6.concederPermissao("TESTE3");
+	}
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -3,25 +3,25 @@ import java.util.Set;
 
 public class Usuario implements ItemPermissao {
 
-    private Long id;
-    private String nome;
-    private Set<String> permissoes = new HashSet<>();
-    
-    public Usuario(Long id, String nome) {
-	this.id = id;
-	this.nome = nome;
-    }
+	private Long id;
+	private String nome;
+	private Set<String> permissoes = new HashSet<>();
 
-    public Long getId() {
-	return id;
-    }
+	public Usuario(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
 
-    public String getNome() {
-	return nome;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    @Override
-    public void concederPermissao(String codigoPermissao) {
-	this.permissoes.add(codigoPermissao);
-    }
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public void concederPermissao(String codigoPermissao) {
+		this.permissoes.add(codigoPermissao);
+	}
 }
